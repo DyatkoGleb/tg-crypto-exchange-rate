5df33353ff493458353b55bc367d2d6d402b5d3d
DyatkoGleb <glebikon@yandex.ru>
2024-06-04 19:42:57 +0300

    v1.1.0 Add the ability to add a bot to channels with certain tickers

--- index.js
+++ index.js
@@ -4,63 +4,100 @@ require('dotenv').config()
 
 
 const bot = new Telegraf(process.env.BOT_KEY)
-const needTokenList = ['BTC', 'ETH', 'TONCOIN']
+const defaultTokenList = ['BTC', 'ETH', 'TONCOIN']
 const UPDATE_TIME = 300_000
+const sentMessages = {}
+const channels = {}
 
 
 async function getPrice(currency) {
     try {
         const response = await axios.get(`https://min-api.cryptocompare.com/data/price?fsym=${currency}&tsyms=USD`)
-
         return response.data.USD
     } catch (error) {
-        console.error('Error fetching price:', error)
-        return 'Error fetching price'
+        console.error('Ошибка при получении цены:', error)
+        return 'Ошибка при получении цены'
     }
 }
 
-async function createMessage() {
+async function createMessage(channelId) {
     let message = '📊\n'
 
-    for (let ticker of needTokenList) {
+    for (let ticker of channels[channelId]) {
         const price = await getPrice(ticker)
 
         if (ticker === 'TONCOIN') {
             ticker = 'TON'
         }
 
-        message += ticker + ': *$' + price + '* ' + '\n'
+        message += `${ticker}: *$${price}* \n`
     }
 
     return message
 }
 
-bot.command('prices', async (ctx) => {
-    let message = await createMessage()
-    let sentMessage = await ctx.replyWithMarkdown(message)
+async function sendMessageToChannels(bot, message, channelId) {
+    try {
+        const admins = await bot.telegram.getChatAdministrators(channelId);
 
-    setInterval(async () => {
-        const updatedMessage = await createMessage()
-
-        if (!updatedMessage) {
-            return
+        if (admins.some(admin => admin.user.id === bot.botInfo.id)) {
+            if (!sentMessages[channelId]) {
+                const sentMessage = await bot.telegram.sendMessage(channelId, message, { parse_mode: 'Markdown' })
+                sentMessages[channelId] = sentMessage.message_id
+            } else {
+                const messageId = sentMessages[channelId]
+                await bot.telegram.editMessageText(channelId, messageId, null, message, { parse_mode: 'Markdown' })
+            }
         }
+    } catch (error) {
+        console.error(`Ошибка при отправке сообщения в канал с ID ${channelId}:`, error)
+    }
+}
 
-        if (updatedMessage !== message) {
-            message = updatedMessage
-            ctx.telegram.editMessageText(
-                sentMessage.chat.id,
-                sentMessage.message_id,
-                null,
-                updatedMessage,
-                { parse_mode: 'Markdown' }
-            ).then((result) => {
-                sentMessage = result
-            }).catch((error) => {
-                console.error('Error editing message:', error);
-            })
+bot.command('send_prices', async () => {
+    for (const channelId in channels) {
+        let message = await createMessage(channelId)
+        await sendMessageToChannels(bot, message, channelId)
+
+        setInterval(async () => {
+            const updatedMessage = await createMessage(channelId)
+
+            if (updatedMessage !== message) {
+                message = updatedMessage
+                await sendMessageToChannels(bot, updatedMessage, channelId)
+            }
+        }, UPDATE_TIME)
+    }
+})
+
+bot.command('add', async (ctx) => {
+    const message = ctx.message.text.split(' ')
+
+    if (message.length === 1) {
+        return bot.telegram.sendMessage(ctx.message.chat.id, `
+            Ожидается сообщение слудующего формата:\n\`/add chatId ticker1 ticker2\`\nНапример:\n\`/add -1002185580962 BTC ETH TONCOIN\`
+        `, { parse_mode: 'Markdown' })
+    }
+
+    if (message.length === 2) {
+        return channels[message[1]] = defaultTokenList
+    }
+
+    for (let i = 2; i < message.length; i++) {
+        if (!channels[message[1]]) {
+            channels[message[1]] = [message[i]]
+        } else {
+            channels[message[1]].push(message[i])
         }
-    }, UPDATE_TIME)
+    }
+})
+
+bot.command('help', async (ctx) => {
+    bot.telegram.sendMessage(ctx.message.chat.id,  'Доступные команды:\n'
+        + '/add - Добавить канал (/add chatId ticker1 ticker2)\n'
+        + '/send_prices - Запустить отправку, перерисовку цен\n'
+        + '/help'
+    )
 })
 
 bot.launch()
